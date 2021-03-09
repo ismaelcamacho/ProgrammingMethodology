bcdcc8a3feec0d108a4f326072257e46c4934a54
Ismael <ismael.camacho1@alu.uclm.es>
2021-03-09 16:51:48 +0100

    ahora si

--- src/Tasks/Task1/main_Task1.java
+++ src/Tasks/Task1/main_Task1.java
@@ -1,6 +1,6 @@
-package Tasks;
+package Tasks.Task1;
 
-public class Main {
+public class main_Task1 {
 
     public static void main(String[] args) {
 	// write your code here
---
+++ src/Tasks/Task2/main_Task2.java
@@ -0,0 +1,4 @@
+package Tasks.Task2;
+
+public class main_Task2 {
+}
