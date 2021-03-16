06074a0782f4d56f56db6d867b25af57334ca77e
Ismael <ismael.camacho1@alu.uclm.es>
2021-03-16 18:21:55 +0100

    recursive

--- src/Tasks/Task1/Third_Algorithm.java
+++ src/Tasks/Task1/Third_Algorithm.java
@@ -2,10 +2,25 @@ package Tasks.Task1;
 
 public class Third_Algorithm {
     public static void main(String[]args){
-
+        int [] numbers = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400};
+        long StartTime = System.currentTimeMillis();
+        for (int i=0; i<numbers.length; i++){
+            System.out.print("The square root of " + numbers[i] + " is= ");
+            System.out.println(SquareRoot(numbers[i]));
+        }
+        System.out.print("Time in milliseconds to execute this algorithm = ");
+        System.out.println(System.currentTimeMillis()-StartTime + "ms");
     }
-    public static double recursive(){
-        int x=0;
-        return x;
+    public static double recursiveSquareRoot(double x, double y){
+        if (x==1){
+            return 1;
+        }
+        else{
+            double result = (y + (x/y)/2);
+            return recursiveSquareRoot(x-1, result);
+        }
+    }
+    public static double SquareRoot(double x){
+        return recursiveSquareRoot(x, x-1);
     }
 }
